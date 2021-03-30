a73538defdfb43f9dd6999e26caf9e5faab05f49
viuleta <74740170+viuleta@users.noreply.github.com>
2021-03-30 10:11:17 -0400

    HW5

---
+++ NNetwork.py
@@ -0,0 +1,107 @@
+####################################
+###  CSDS_435 HW5, Due : 3/30/21 ###
+###  Maryam Ghasemian (mxg708)   ###
+####################################
+from math import exp
+from random import seed
+from random import random
+
+# Initialize a network
+def init_network(inputs_num, hidden_num, output_num):
+    network = list ()
+    hidden_layer = [{'weights': [random () for i in range (inputs_num + 1)]} for i in range (hidden_num)]
+    network.append (hidden_layer)
+    output_layer = [{'weights': [random () for i in range (hidden_num + 1)]} for i in range (output_num)]
+    network.append (output_layer)
+    return network
+
+
+# Calculate neuron activation for an input
+def activate(weights, inputs):
+    activation = weights[-1]
+    for i in range (len (weights) - 1):
+        activation += weights[i] * inputs[i]
+    return activation
+
+
+# sigmoid function
+def sigmoid(activation):
+    return 1.0 / (1.0 + exp (-activation))
+
+
+# Forward propagate
+def forward_propagate(network, row):
+    inputs = row
+    for layer in network:
+        new_inputs = []
+        for neuron in layer:
+            activation = activate (neuron['weights'], inputs)
+            neuron['output'] = sigmoid (activation)
+            new_inputs.append (neuron['output'])
+        inputs = new_inputs
+    return inputs
+
+
+# Calculate the derivative of an neuron output
+def derivative(output):
+    derivative = output * (1.0 - output)
+    return derivative
+
+
+# Backpropagate error and store in neurons
+def backward_propagate_error(network, expected):
+    for i in reversed (range (len (network))):
+        layer = network[i]
+        errors = list ()
+        if i != len (network) - 1:
+            for j in range (len (layer)):
+                error = 0.0
+                for neuron in network[i + 1]:
+                    error += (neuron['weights'][j] * neuron['delta'])
+                errors.append (error)
+        else:
+            for j in range (len (layer)):
+                neuron = layer[j]
+                errors.append (expected[j] - neuron['output'])
+        for j in range (len (layer)):
+            neuron = layer[j]
+            neuron['delta'] = errors[j] * derivative (neuron['output'])
+
+
+# Update network weights with error
+def update_weights(network, row, l_rate):
+    for i in range (len (network)):
+        inputs = row[:-1]
+        if i != 0:
+            inputs = [neuron['output'] for neuron in network[i - 1]]
+        for neuron in network[i]:
+            for j in range (len (inputs)):
+                neuron['weights'][j] += l_rate * neuron['delta'] * inputs[j]
+            neuron['weights'][-1] += l_rate * neuron['delta']
+
+
+# Train a network for a fixed number of iterations
+def train_network(network, train, l_rate, n_iteration, output_num):
+    for iteration in range (n_iteration):
+        sum_error = 0
+        for row in train:
+            outputs = forward_propagate (network, row)
+            expected = [0 for i in range (output_num)]
+            expected[row[-1]] = 1
+            sum_error += sum ([(expected[i] - outputs[i]) ** 2 for i in range (len (expected))])
+            backward_propagate_error (network, expected)
+            update_weights (network, row, l_rate)
+        print ('>iteration=%d, learning rate=%.3f, error=%.3f' % (iteration, l_rate, sum_error))
+
+
+# Test training Backpropagate algorithm
+seed (1)
+dataset = [[0.6, 0.1, 0]]
+lr = 0.1 # learning rate
+print('data-len = ', len(dataset[0]))
+inputs_num = len (dataset[0]) - 1
+output_num = len (set ([row[-1] for row in dataset]))
+network = init_network (inputs_num, 3, output_num)
+train_network (network, dataset, lr, 5, output_num)
+for layer in network:
+    print (layer)
